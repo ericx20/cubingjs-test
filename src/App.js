739353d1eedee57d46834dfd16fa3e66ed495c1a
Eric Xu <ericxu2048@gmail.com>
2022-12-18 16:22:10 -0500

    Add minimal example of cubing/search

--- src/App.js
+++ src/App.js
@@ -1,6 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
 
+import { random333State } from "cubing/search"
+import { experimentalSolve3x3x3IgnoringCenters } from "cubing/search";
+import { KState } from "cubing/kpuzzle"
+
+async function generateEOSolvedScramble() {
+  const { kpuzzle, stateData } = await random333State()
+  const newStateData = {
+    ...stateData,
+    EDGES: {
+      // TODO: make this an n-flip array
+      orientation: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      pieces: stateData.EDGES.pieces
+    }
+  }
+  const newPuzzle = new KState(kpuzzle, newStateData)
+  const solution = await experimentalSolve3x3x3IgnoringCenters(newPuzzle)
+  return solution.invert()
+}
+
+console.log(generateEOSolvedScramble().toString())
+
 function App() {
   return (
     <div className="App">
